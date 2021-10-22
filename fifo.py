f528f55283fc5f928a29bb14e0519e560a3fabcb
Albert Calasanz <acalasanzs@gmail.com>
2021-10-22 10:02:10 +0200

    D

--- fifo.py
+++ fifo.py
@@ -8,36 +8,34 @@ chars = [x for x in string.ascii_uppercase]
 
 abc = Assgn(Ar2Dict(["cuants procesos (max: 26)"],"units"),vals=range(1,27),rules=[False,False,True])
 abc.input()
-t = Assgn(Ar2Dict(chars[:abc.ans],"t"),conj="as",rules=[False,False,True])
+t = Assgn(Ar2Dict(chars[:abc.ans],"t"),conj="com a",rules=[False,False,True])
 t.input()
-ti = Assgn(Ar2Dict(chars[:abc.ans],"ti"),conj="as",rules=[False,False,True])
+ti = Assgn(Ar2Dict(chars[:abc.ans],"ti"),conj="com a",rules=[False,False,True])
 ti.input()
 
 tf = np.zeros((len(chars[:abc.ans]),))
 te = np.zeros((len(chars[:abc.ans]),))
 """ process = np.array(chars[:abc.ans]).reshape(len(chars[:abc.ans]),1)
 print(process) """
-
+ti = ti.array
+t = t.array
 data = {}
 def update():
     for idx,x in enumerate(chars[:abc.ans]):
-        data[x] = [t.array[idx],ti.array[idx],tf[idx],te[idx]]
-
+        data[x] = [t[idx],ti[idx],tf[idx],te[idx]]
+update()
 dt = pd.DataFrame(data)
 print(dt)
 print("0:t,1:ti,2:tf,3:te")
 print("Zeros are x")
-
-current = ti.array
+current = ti
 quantum = 0
-
-for idx,x in enumerate(ti.array):
+for idx,x in enumerate(ti):
     first = min(current)
-    tf[ti.array.index(first)] = quantum + t.array[ti.array.index(first)]
-    quantum += t.array[ti.array.index(first)]
-    current.remove(first)
-
-print(tf)
+    tf[ti.index(first)] = quantum + t[ti.index(first)]
+    quantum += t[ti.index(first)]
+    #del current[ti.index(first)]
+print(ti)
 update()
 print(pd.DataFrame(data))
 print("0:t,1:ti,2:tf,3:te")
