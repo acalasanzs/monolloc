41f9fc6c9cd9dc4f3e3a9a09bac26cd859dc11f9
Albert Calasanz <acalasanzs@gmail.com>
2021-10-23 08:26:19 +0200

    comment

--- matrix/fifo.py
+++ matrix/fifo.py
@@ -6,6 +6,7 @@ import string
 print("First Input First Output")
 "There's an errror with self.vals that return 0 array"
 
+# Get ABCDEFGH...
 chars = [x for x in string.ascii_uppercase]
 
 # Set values of inputs
@@ -17,18 +18,22 @@ t.input()
 ti = Assgn(Ar2Dict(chars[:abc.ans],"ti"),conj="com a",rules=[True,False,True])
 ti.input()
 
+# Set tf and te to 0
 tf = np.zeros((len(chars[:abc.ans]),)).tolist()
 te = np.zeros((len(chars[:abc.ans]),)).tolist()
 
+# Simplify
 ti = ti.array
 t = t.array
+
+# Update chart
 data = {}
 def update():
     for idx,x in enumerate(chars[:abc.ans]):
         data[x] = [t[idx],ti[idx],tf[idx],te[idx]]
 update()
-dt = pd.DataFrame(data)
 
+# Calculate tf with quantum and ti copy to know wich is first
 current = ti.copy()
 quantum = -1
 for idx,x in enumerate(ti):
@@ -36,35 +41,48 @@ for idx,x in enumerate(ti):
     tf[ti.index(first)] = quantum + t[ti.index(first)]
     quantum += t[ti.index(first)]
     current.remove(first)
+
+# Make a 2D chart of 0 of length sum(t)
 table = np.zeros((len(chars[:abc.ans]),sum(t)))
 tabley = 0
 current = ti.copy()
 print(" | ".join([str(tuple(a)) for a in zip(ti,tf)]))
 
+# Calculate wait or run
 for idx,k in enumerate(ti):
+    # The first is wich is min TI and and its tf
     first = (min(current),tf[ti.index(min(current))])
+    # Index of current x for chart is first[0]
     tablex = first[0]
     for x in range(first[0],first[1]+1):
         try:
+            # if (x-1,y) is 1 or 2
             assert table[tabley-1][tablex] in (1,2)
+            # Convert (x,y) to 2
             table[tabley][tablex] = 2
+            # Add to wait number
             te[idx] += 1
         except:
             try:
                 table[tabley][tablex] = 1
             except IndexError:
+                # If the above code bounds the limits here breaks loop
                 break
         tablex += 1
     else:
         tabley += 1
+    # remove from temp
     current.remove(min(current))
 
-
+# Reverse table and update chart
 table = table[::-1]
 update()
 print(pd.DataFrame(data))
 print("0:t,1:ti,2:tf,3:te")
+# Convert to int
 print(table.astype(int))
+
+# Save as csv
 with open('data.csv', mode='w',newline='') as file:
     file = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
     for x in table:
